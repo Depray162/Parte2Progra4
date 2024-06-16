8cc652a673feb20ffcf49b0caa676681b5d9f7ff
Depray162 <fabricio1quesada@gmail.com>
2024-06-16 14:43:28 -0600

    a

--- FronEndMariaDB/src/app/app.routes.ts
+++ FronEndMariaDB/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-
+import { LoginDocComponent } from './components/login-doc/login-doc.component';
 
 export const routes: Routes = [
-    {path:'login',component:LoginComponent}
-
+    {path:'login',component:LoginComponent},
+    {path:'loginDoc',component:LoginDocComponent}
 
 ];
---
+++ FronEndMariaDB/src/app/components/login-doc/login-doc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-doc',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-doc.component.html',
+  styleUrl: './login-doc.component.css'
+})
+export class LoginDocComponent {
+
+}
