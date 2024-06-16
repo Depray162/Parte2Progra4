fc6758448d4cee0eafb5460635c36d2b3eff8c78
KevinSalbra <kevinsalazar74.ksb@gmail.com>
2024-06-16 14:42:53 -0600

    actualizacion de archivos

--- FronEndMariaDB/src/app/app.routes.ts
+++ FronEndMariaDB/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-
+import { LandingComponentsComponent } from './landing-components/landing-components.component';
 
 export const routes: Routes = [
-    {path:'login',component:LoginComponent}
-
-
+    {path:'',component:LandingComponentsComponent},
+    {path:'login',component:LoginComponent},
+    {path:'landing-components',component:LandingComponentsComponent}
 ];
