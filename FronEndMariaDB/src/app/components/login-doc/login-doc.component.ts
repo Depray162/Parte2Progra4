93974dea8652db3a9dac53b2ea10bf7daead4a74
KevinSalbra <kevinsalazar74.ksb@gmail.com>
2024-06-16 17:38:12 -0600

    conexion de logins

--- FronEndMariaDB/src/app/components/login-doc/login-doc.component.ts
+++ FronEndMariaDB/src/app/components/login-doc/login-doc.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-doc',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './login-doc.component.html',
   styleUrl: './login-doc.component.css'
 })
--- FronEndMariaDB/src/app/components/login/login.component.ts
+++ FronEndMariaDB/src/app/components/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import { LoginDocComponent } from '../login-doc/login-doc.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, LoginDocComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  public identity:any;
 }
